1947629736a1db2e8a3b2c49e5a2f7de49d4e99d
yasser296 <98412954+yasser296@users.noreply.github.com>
2022-01-25 19:15:52 +0200

    Add files via upload

---
+++ send_to_trash/send2trash.py
@@ -0,0 +1,6 @@
+import send2trash
+
+send2trash.send2trash("E:\python_projects\send_to_trashtext_to_trash.txt")
+
+#import send2trash
+#send2trash.send2trash.('C:\\Users\CodersLegacy\testdirectory')		 						
